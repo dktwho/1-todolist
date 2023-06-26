6662357a8dfb4a4c090e9f9d8c67b752728c1b40
Alexander Korobov <dktwho@mail.ru>
2023-06-26 12:09:39 +0300

    Add one more Todolist with typed props, modified gitignore

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,45 @@
 import React from "react";
 import "./App.css";
-import { Todolist } from "./Todolist";
+import {Todolist} from "./Todolist";
 
 export type TaskType = {
-  id: number;
-  title: string;
-  isDone: boolean;
+    id: number;
+    title: string;
+    isDone: boolean;
 };
 
 function App() {
-  const title = "what to learn";
-  const title2 = 100500;
-  const title3 = true;
+    const title = "what to learn";
+    const title2 = 100500;
+    const title3 = true;
 
-  const tasks1 = [
-    { id: 1, title: "HTML&CSS", isDone: true },
-    { id: 2, title: "JS", isDone: true },
-    { id: 3, title: "ReactJS", isDone: false },
-    { id: 4, title: "VueJS", isDone: false },
-    { id: 5, title: "Angular", isDone: true },
-    { id: 6, title: "Svelte", isDone: true },
-  ];
-  const tasks2 = [
-    { id: 1, title: "Hello world", isDone: true },
-    { id: 2, title: "I am Happy", isDone: false },
-    { id: 3, title: "Yo", isDone: false },
-  ];
+    const tasks1 = [
+        {id: 1, title: "HTML&CSS", isDone: true},
+        {id: 2, title: "JS", isDone: true},
+        {id: 3, title: "ReactJS", isDone: false},
+        {id: 4, title: "VueJS", isDone: false},
+        {id: 5, title: "Angular", isDone: true},
+        {id: 6, title: "Svelte", isDone: true},
+    ];
+    const tasks2 = [
+        {id: 1, title: "Hello world", isDone: true},
+        {id: 2, title: "I am Happy", isDone: false},
+        {id: 3, title: "Yo", isDone: false},
+    ];
 
-  return (
-    <div className="App">
-      <Todolist title={title} title2={title2} tasks={tasks1} />
-      <Todolist title={`${title} 2`} title3={title3} tasks={tasks2} />
-    </div>
-  );
+    const tasks3 = [
+        {id: 1, title: "Redux", isDone: false},
+        {id: 2, title: "Thunk/Saga", isDone: false},
+        {id: 3, title: "Redux Tkt", isDone: false},
+    ];
+
+    return (
+        <div className="App">
+            <Todolist title={title} title2={title2} tasks={tasks1}/>
+            <Todolist title={`${title} 2`} title3={title3} tasks={tasks2}/>
+            <Todolist title={`${title} 2`} title3={title3} tasks={tasks3}/>
+        </div>
+    );
 }
 
 export default App;
--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -1,39 +1,39 @@
 import React from "react";
-import { TaskType } from "./App";
+import {TaskType} from "./App";
 
 type PropsTypeTitle = {
-  title: string;
-  title2?: number;
-  title3?: boolean;
-  // tasks: Array<TaskType>; generics
-  tasks: TaskType[]; // new syntax for array
+    title: string;
+    title2?: number;
+    title3?: boolean;
+    // tasks: Array<TaskType>; generics
+    tasks: TaskType[]; // new syntax for array
 };
 
-export const Todolist = ({ title, title2, title3, tasks }: PropsTypeTitle) => {
-  return (
-    <div>
-      <h3>
-        {title} - {title2} - {title3}
-      </h3>
-      <div>
-        <input />
-        <button>+</button>
-      </div>
-      <ul>
-        {tasks.map((el) => {
-          return (
-            <li key={el.id}>
-              <input type="checkbox" checked={el.isDone} readOnly />
-              <span>{el.title}</span>
-            </li>
-          );
-        })}
-      </ul>
-      <div>
-        <button>All</button>
-        <button>Active</button>
-        <button>Completed</button>
-      </div>
-    </div>
-  );
+export const Todolist = ({title, title2, title3, tasks}: PropsTypeTitle) => {
+    return (
+        <div>
+            <h3>
+                {title} - {title2} - {title3}
+            </h3>
+            <div>
+                <input/>
+                <button>text inside button</button>
+            </div>
+            <ul>
+                {tasks.map((el) => {
+                    return (
+                        <li key={el.id}>
+                            <input type="checkbox" checked={el.isDone} readOnly/>
+                            <span>{el.title}</span>
+                        </li>
+                    );
+                })}
+            </ul>
+            <div>
+                <button>All</button>
+                <button>Active</button>
+                <button>Completed</button>
+            </div>
+        </div>
+    );
 };
